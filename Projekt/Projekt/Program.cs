47159fa5f069d8a311141fa07f7abe932e3d9c1c
229872 <bartek.dygasinski00@gmail.com>
2022-03-25 14:32:31 +0100

    Kompulujący się program

---
+++ Projekt/Projekt/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace zadanie0
+{
+    public class Zadanie0
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("2 * 2 = " + Multiply(2,2));
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
